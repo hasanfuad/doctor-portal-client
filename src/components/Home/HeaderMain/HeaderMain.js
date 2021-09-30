3cb165f70a17c762ca6db5269bb2ce2be3f5750d
hasanfuad <hasanmdfuad@hotmail.com>
2021-09-30 10:13:51 +0600

    card design done

--- src/components/Home/HeaderMain/HeaderMain.js
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React from "react";
 import chair from "../../images/Chair.png";
 
 const HeaderMain = () => {
-    return (
-        <main style={{height: '600px'}} className="row d-flex align-items-center">
-            <div className="col-md-4 offset-md-1">
-                <h1 style={{color: "#3a4256"}}>Your Smile <br/> Starts Here</h1>
-                <p className="text-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <button style={{backgroundColor: "#1CC7C1", border: "none"}} className="btn btn-primary">GET APPOINTMENT</button>
-            </div>
-            <div className="col-md-6">
-                <img src={chair} alt="" className="img-fluid"/>
-
-            </div>
-        </main>
-    );
+  return (
+    <main style={{ height: "600px" }} className="row d-flex align-items-center">
+      <div className="col-md-4 offset-md-1">
+        <h1 style={{ color: "#3a4256" }}>
+          Your Smile <br /> Starts Here
+        </h1>
+        <p className="text-secondary">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <button
+          style={{ backgroundColor: "#1CC7C1", border: "none" }}
+          className="btn btn-primary"
+        >
+          GET APPOINTMENT
+        </button>
+      </div>
+      <div className="col-md-6">
+        <img src={chair} alt="" className="img-fluid" />
+      </div>
+    </main>
+  );
 };
 
 export default HeaderMain;
